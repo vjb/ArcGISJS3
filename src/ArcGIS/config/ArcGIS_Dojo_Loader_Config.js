e167e4fa2ddd091d852ce0fe979d1b3ad5ba0eef
vincent beltrani <vincent.beltrani@mendix.com>
2019-05-28 13:47:19 -0400

    mapped libraries using config options to require.  I think this is the right way to do this.  It separates the Mendix Dojo libraries from the ArcGIS Libraries

--- src/ArcGIS/config/ArcGIS_Dojo_Loader_Config.js
+++ src/ArcGIS/config/ArcGIS_Dojo_Loader_Config.js
@@ -5,24 +5,23 @@ should include this configuration -- it will tell the Dojo Loader where to look 
 that start with <name>.  Otherwise, Mendix looks for them under mxclientsystem
 */
 
-define(
-    {
-        packages: [{
-                name: "esri",
-                location: 'https://dsraenterprise2.canadacentral.cloudapp.azure.com/portal/jsapi/jsapi/esri'
-            },
-            {
-                name: "dojox",
-                location: 'https://dsraenterprise2.canadacentral.cloudapp.azure.com/portal/jsapi/jsapi/dojox'
-            },
-            {
-                name: "dojo",
-                location: 'https://dsraenterprise2.canadacentral.cloudapp.azure.com/portal/jsapi/jsapi/dojo'
-            },
-            {
-                name: "moment",
-                location: 'https://dsraenterprise2.canadacentral.cloudapp.azure.com/portal/jsapi/jsapi/moment'
-            },
-        ]
-    }
-);
+
+define({
+    packages: [{
+            name: "esri",
+            location: 'https://dsraenterprise2.canadacentral.cloudapp.azure.com/portal/jsapi/jsapi/esri'
+        },
+        {
+            name: "dojox",
+            location: 'https://dsraenterprise2.canadacentral.cloudapp.azure.com/portal/jsapi/jsapi/dojox'
+        },
+        {
+            name: "dojo",
+            location: 'https://dsraenterprise2.canadacentral.cloudapp.azure.com/portal/jsapi/jsapi/dojo'
+        },
+        {
+            name: "moment",
+            location: 'https://dsraenterprise2.canadacentral.cloudapp.azure.com/portal/jsapi/jsapi/moment'
+        },
+    ]
+});
